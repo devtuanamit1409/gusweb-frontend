5e1f0f2913df8efee266ca9ab839df105c1da539
quoccuongDCT <quoccuong.mwg1168@gmail.com>
2024-09-19 13:59:28 +0700

    19/09/2024-13h58p
    update Header, update Navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,19 @@ import Image from 'next/image';
 
 const Header = () => {
     return (
-        <div className="absolute top-0 left-0 right-0 bottom-0 ">
-            <div className='flex items-center justify-center max-w-[1140px] mx-auto text-center'>
-                <div className='flex-1' >
-                    <h1 className='home-banner-title'>AMIT GROUP</h1>
-                    <h3 className='home-banner-description'>GIẢI PHÁP CÔNG NGHỆ THÔNG TIN BỀN VỮNG</h3>
+        <div className="absolute top-14 left-0 right-0 bottom-0">
+            <div className='flex items-center justify-center max-w-[1140px] mx-auto text-left'>
+                <div className='flex-1 ' >
+                    <h2 className='text-[#fefefe] text-2xl home-banner-preamble'>CHÚNG TÔI LÀ</h2>
+                    <h1 className='home-banner-title text-[76px]'>AMIT GROUP</h1>
+                    <h3 className='home-banner-description text-[32px]'>GIẢI PHÁP CÔNG NGHỆ THÔNG TIN BỀN VỮNG</h3>
                     <p className='home-banner-content'>AMIT luôn hướng tới các giải pháp toàn diện, với tinh thần trách nhiệm cao và hiệu quả chi phí, nhằm mang lại giá trị lâu dài cho sự phát triển bền vững của khách hàng và doanh nghiệp.</p>
+                    <button
+                        type="button"
+                        className="text-white ant-btn focus:outline-none font-medium rounded-md text-sm hidden lg:block md:w-auto md:order-1"
+                    >
+                        Tải profiled
+                    </button>
                 </div>
                 <div className="flex-1">
                     <Image
@@ -17,9 +24,6 @@ const Header = () => {
                         alt="Header Banner"
                         width={546}
                         height={479}
-                        className="object-right"
-                        loading="lazy"
-                        decoding="async"
                     />
                 </div>
             </div>
