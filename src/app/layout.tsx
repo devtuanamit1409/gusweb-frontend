90f927da84a59eb63956137e03dbdb658c25c0e6
quoccuongDCT <quoccuong.mwg1168@gmail.com>
2024-09-26 15:35:59 +0700

    26/09/2024

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,8 @@
-import Footer from "@/components/Footer";
+
 import Navbar from "@/components/Navbar";
 import type { Metadata } from "next";
 import "../styles/globals.css";
-import DesignGusweb from "@/components/DesignGusweb";
-import Inflation from "@/components/Inflation";
-import GUSWEB from "@/components/GUSWEB";
-import WebsiteCard from "@/components/WebsiteCard";
-import About from "@/components/About";
-import Principle from "@/components/Principle";
+
 
 // import { AntdRegistry } from '@ant-design/nextjs-registry';
 // const inter = Inter({ subsets: ["latin"] });
@@ -26,13 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Navbar />
-        <Inflation />
-        <GUSWEB />
-        <WebsiteCard />
-        <About />
-        <DesignGusweb />
-        <Principle />
-        <Footer />
+
         {/* {children} */}
       </body>
     </html>
