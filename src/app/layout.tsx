7aaad9759757dd5e3fef8f4fed796e7eafd12fd4
PhamBaBac <bacphamba082@gmail.com>
2024-10-02 13:37:52 +0700

    feat: ResponsibilityComponent

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Navbar />
-        {children}
+        <main className="mt-[88px]">{children}</main>
         <Footer />
       </body>
     </html>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,18 +8,33 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        desktop: "1440px",
+        laptop: "1024px",
+        tablet: "744px",
+        mobile: "360px",
+      },
       fontFamily: {
-        bricolage: ['"Bricolage Grotesque"', 'sans-serif'],
+        bricolage: ['"Bricolage Grotesque"', "sans-serif"],
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+
+      animation: {
+        "expand-circle": "expand-circle 1.5s ease-out infinite",
+      },
+      keyframes: {
+        "expand-circle": {
+          "0%": { transform: "scale(0.5)", opacity: "1" },
+          "100%": { transform: "scale(2.5)", opacity: "0" },
+        },
+      },
     },
   },
   plugins: [],
-  
 };
 
 export default config;
