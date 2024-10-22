1bb976bc69f35fa047dabc7d6314052a3b6cd8d6
PhamBaBac <bacphamba082@gmail.com>
2024-10-22 08:26:20 +0700

    feat: data-aos

--- src/components/IdeaCard.tsx
+++ src/components/IdeaCard.tsx
@@ -1,31 +1,16 @@
-import React from "react";
-import { Card, Flex } from "antd";
+'use client';
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 const IdeaCard: React.FC<any> = ({ solution }) => {
-  const content = {
-    Premble: " CÁC GIẢI PHÁP CỦA GUSWEB",
-    title: "Biến ý tưởng thành sản phẩm hoàn thiện",
-  };
-  const items = [
-    {
-      title: "Đổi mới giao diện",
-      description:
-        "Biến giao diện người dùng đã lỗi thời trở nên hiện đại và dễ sử dụng hơn nhờ kết hợp các tiêu chuẩn UX UI trong thiết kế.",
-    },
-    {
-      title: "Tối ưu hóa hiệu suất",
-      description:
-        "Cải thiện tốc độ tải trang và tối ưu hóa hiệu suất ứng dụng giúp người dùng có trải nghiệm tốt hơn.",
-    },
-    {
-      title: "Phát triển tính năng mới",
-      description:
-        "Thêm các tính năng mới giúp nâng cao giá trị sản phẩm và đáp ứng nhu cầu của khách hàng.",
-    },
-  ];
+ useEffect(() => {
+   AOS.init({ once: true });
+   setTimeout(() => {}, 1000);
+ }, []);
   return (
     <div className="w-full bg-gradient-to-r from-[#00d2ff] to-[#3a7bd5]  items-center flex ">
       <div className="custom-container flex flex-col gap-6 mobile:gap-8 max-w-[1116px] laptop:py-[80px]  py-[66px] px-4   items-center ">
-        <div className="laptop:h-[180px] gap-4  flex flex-col  items-center text-center w-full">
+        <div className="laptop:h-[180px] gap-4  flex flex-col  items-center text-center w-full" data-aos="zoom-in">
           <span className="laptop:max-w-[329px] h-[38px]  text-[#FFFFFF] text-[24px] leading-[28.8px] font-bricolage font-bold w-full">
             CÁC GIẢI PHÁP CỦA GUSWEB
           </span>
@@ -33,7 +18,7 @@ const IdeaCard: React.FC<any> = ({ solution }) => {
             {solution.title}
           </h1>
         </div>
-        <div className=" px-4 tablet:px-0  gap-6 flex flex-wrap laptop:flex-nowrap  justify-center max-w-[1116px]">
+        <div className="px-4 tablet:px-0 gap-6 flex flex-wrap laptop:flex-nowrap justify-center max-w-[1116px]" data-aos="fade-up">
           {solution.items &&                
             solution.items.map((item: any, index: number) => (
                 <div
